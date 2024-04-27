e227e7d3277443df708602173e61aa7edce3ecb7
PrajwalSh0206 <prajwalshetty57@gmail.com>
2024-04-27 17:32:33 +0530

    login page created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
+import "./App.scss";
 import "./scss/common.scss";
+import { Outlet } from "react-router-dom";
 
 const App: React.FC = () => {
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <Outlet></Outlet>
+    </div>
+  );
 };
 
 export default App;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,20 +1,31 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/store";
+import Login from "./pages/login/Login";
 
 const rootElement = document.getElementById("root");
 if (!rootElement) {
   throw new Error("Root element not found in the document!");
 }
 
-ReactDOM.render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Login></Login>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(rootElement).render(
   <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
-  </Provider>,
-  rootElement
+    <RouterProvider router={router}></RouterProvider>
+  </Provider>
 );
