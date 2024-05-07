eaba53bf4e5cbec03bfeed5c8d9ee0397722a327
PrajwalSh0206 <prajwalshetty57@gmail.com>
2024-05-07 07:30:18 +0530

    filter and logout added

--- frontend/src/pages/chat/components/SideBar.tsx
+++ frontend/src/pages/chat/components/SideBar.tsx
@@ -1,21 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./SideBar.scss";
 import { useDispatch, useSelector } from "react-redux";
 import { storeSelectedRecieverDetails } from "../../../store/reducers/recieverReducer";
 import { RootState } from "../../../store/store";
-
-interface socketUserDto {
-  userId: string;
-  username: string;
-  online: boolean;
-  socketId?: string;
-  messages: Array<any>;
-}
+import { ReceiverDto, socketUserDto } from "../../../../dto/socket";
+import { useNavigate } from "react-router-dom";
 
 const SideBar: React.FC = () => {
   const recieverDetails = useSelector((state: RootState) => state.receiever.receivers);
-
+  const [userDetails, setUserDetails] = useState<Array<ReceiverDto>>([]);
+  const [searchText, setSearchText] = useState("");
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (recieverDetails.length) setUserDetails(recieverDetails);
+  }, [recieverDetails]);
+
   const setReciever = (receiver: socketUserDto) => {
     const payload = {
       userId: receiver.userId,
@@ -26,6 +27,18 @@ const SideBar: React.FC = () => {
     };
     dispatch(storeSelectedRecieverDetails(payload));
     console.log(recieverDetails, "new connection");
+  };
+
+  const handleSearch = (e: any) => {
+    const name = e.target.value;
+    setSearchText(name);
+    const filterDetails = recieverDetails.filter((value) => value.username.toUpperCase().includes(name.toUpperCase()));
+    setUserDetails(filterDetails);
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/");
   };
 
   return (
@@ -52,50 +65,40 @@ const SideBar: React.FC = () => {
             />
           </svg>
 
-          <input type="text" placeholder="Search Friends" />
+          <input type="text" value={searchText} onChange={(e) => handleSearch(e)} placeholder="Search Friends" />
         </div>
       </div>
       <div id="users">
-        {recieverDetails.map((value) => {
-          return (
-            <button className="contact" key={value.userId} onClick={() => setReciever(value)}>
-              <img src={`https://api.dicebear.com/8.x/shapes/svg?seed=${value.username}`} alt={value.username} />
-              <div className="userInfo">
-                <p>{value.username}</p>
-                {value.messages.length > 0 && (
-                  <div className="recentMessage">
-                    <small>{value.messages[value.messages.length - 1].content.substring(0, 30)} - </small>
-                    <small>
-                      {new Intl.DateTimeFormat("en-US", { hour: "numeric", minute: "numeric" }).format(
-                        new Date(value.messages[value.messages.length - 1].timeStamp)
-                      )}
-                    </small>
-                  </div>
-                )}
-              </div>
-              <div className={`${value.online ? "green" : ""} status`}></div>
-              {/* Online Status */}
-            </button>
-          );
-        })}
+        {userDetails.length > 0 &&
+          userDetails.map((value) => {
+            return (
+              <button className="contact" key={value.userId} onClick={() => setReciever(value)}>
+                <img src={`https://api.dicebear.com/8.x/shapes/svg?seed=${value.username}`} alt={value.username} />
+                <div className="userInfo">
+                  <p>{value.username}</p>
+                  {value.messages.length > 0 && (
+                    <div className="recentMessage">
+                      <small>{value.messages[value.messages.length - 1].content.substring(0, 30)} - </small>
+                      <small>
+                        {new Intl.DateTimeFormat("en-US", { hour: "numeric", minute: "numeric" }).format(
+                          new Date(value.messages[value.messages.length - 1].timeStamp)
+                        )}
+                      </small>
+                    </div>
+                  )}
+                </div>
+                <div className={`${value.online ? "green" : ""} status`}></div>
+                {/* Online Status */}
+              </button>
+            );
+          })}
       </div>
       <div className="btn-grp">
-        <button id="logout">
+        <button id="logout" onClick={handleLogout}>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3" />
           </svg>
           <p>Logout</p>
-        </button>
-        <button id="setting">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-            <path
-              fillRule="evenodd"
-              d="M11.078 2.25c-.917 0-1.699.663-1.85 1.567L9.05 4.889c-.02.12-.115.26-.297.348a7.493 7.493 0 0 0-.986.57c-.166.115-.334.126-.45.083L6.3 5.508a1.875 1.875 0 0 0-2.282.819l-.922 1.597a1.875 1.875 0 0 0 .432 2.385l.84.692c.095.078.17.229.154.43a7.598 7.598 0 0 0 0 1.139c.015.2-.059.352-.153.43l-.841.692a1.875 1.875 0 0 0-.432 2.385l.922 1.597a1.875 1.875 0 0 0 2.282.818l1.019-.382c.115-.043.283-.031.45.082.312.214.641.405.985.57.182.088.277.228.297.35l.178 1.071c.151.904.933 1.567 1.85 1.567h1.844c.916 0 1.699-.663 1.85-1.567l.178-1.072c.02-.12.114-.26.297-.349.344-.165.673-.356.985-.57.167-.114.335-.125.45-.082l1.02.382a1.875 1.875 0 0 0 2.28-.819l.923-1.597a1.875 1.875 0 0 0-.432-2.385l-.84-.692c-.095-.078-.17-.229-.154-.43a7.614 7.614 0 0 0 0-1.139c-.016-.2.059-.352.153-.43l.84-.692c.708-.582.891-1.59.433-2.385l-.922-1.597a1.875 1.875 0 0 0-2.282-.818l-1.02.382c-.114.043-.282.031-.449-.083a7.49 7.49 0 0 0-.985-.57c-.183-.087-.277-.227-.297-.348l-.179-1.072a1.875 1.875 0 0 0-1.85-1.567h-1.843ZM12 15.75a3.75 3.75 0 1 0 0-7.5 3.75 3.75 0 0 0 0 7.5Z"
-              clipRule="evenodd"
-            />
-          </svg>
-
-          <p>Setting</p>
         </button>
       </div>
     </aside>
